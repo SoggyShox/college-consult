19d1c77195011e9d2c78456640be4832ac2d7d9f
Austin F <austinfujimori123@gmail.com>
2021-01-25 18:53:15 -0800

    my first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          hi my name is austin1
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
