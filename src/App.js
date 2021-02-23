d4d7ad60f86b2349e631a1fc697c1984075dc590
Austin F <austinfujimori123@gmail.com>
2021-02-23 11:58:50 -0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,78 +1,203 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
-import clsx from 'clsx';
-import CardHeader from '@material-ui/core/CardHeader';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import clsx from "clsx";
+import Container from "@material-ui/core/Container";
+import CardHeader from "@material-ui/core/CardHeader";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from "@material-ui/core/styles";
 
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import blue from '@material-ui/core/colors/blue';
-import grey from '@material-ui/core/colors/grey';
-import logo from './logo.svg';
-import logo2 from './logo2.jpg';
-import './App.css';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import blue from "@material-ui/core/colors/blue";
+import grey from "@material-ui/core/colors/grey";
+import logo from "./logo.svg";
+import logo2 from "./logo2.jpg";
+import "./App.css";
 
 const appTheme = createMuiTheme({
   palette: {
     primary: {
-      main: blue[400],
+      main: blue[400]
     },
     secondary: {
-      main: '#f4FFFFFF',
-    },
-  },
+      main: "#f4FFFFFF"
+    }
+  }
 });
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   app: {
-    textAlign: 'left',
+    textAlign: "left"
   },
   appBar: {
-    backgroundColor: '#282828',
-    color: '#5DD1F1'
+    backgroundColor: "#282828",
+    color: "#5DD1F1"
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  gridthing: {
+    marginTop: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     width: theme.spacing(25),
-    height: theme.spacing(25),
+    height: theme.spacing(25)
   },
   media: {
-    height: 200,
+    height: 200
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-
+    transform: "rotate(180deg)"
+  }
 }));
 
+function GridPerson({ person }) {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = name => {
+    console.log(name);
+    setOpen(false);
+  };
+
+  const [connectButtonOpen, setConnectButtonOpen] = React.useState(false);
+
+  const connectButtonHandleOpen = () => {
+    setConnectButtonOpen(true);
+  };
+
+  const connectButtonHandleClose = () => {
+    setConnectButtonOpen(false);
+  };
+
+  return (
+    <Grid item id={person.id} xs={12} sm={6} md={4}>
+      <Card className={classes.root} variant="outlined">
+        <CardActionArea onClick={handleClickOpen}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {person.name}
+            </Typography>
+            <Typography variant="body2" color="black" component="p">
+              {person.description}
+            </Typography>
+          </CardContent>
+          <CardMedia component="img" height="200" image={person.logo} />
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" onClick={handleClickOpen}>
+            Connect
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">
+          Connect with {person.name}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Connect with a student from your dream college for a short 30 minute
+            session where you are available to ask any question you would like.
+          </DialogContentText>
+          <DialogContentText>
+            Some good questions: What camps or extracurriculars did you take
+            that helped you get into the college? How did you prepare for them?
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Full Name"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleClose("")} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              handleClose(person.name);
+              connectButtonHandleOpen();
+            }}
+            color="primary"
+          >
+            Connect
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={connectButtonOpen}
+        onClose={connectButtonHandleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Success!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            We have recieved your request and will follow up shortly.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={connectButtonHandleClose} color="primary" autoFocus>
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
+}
 
 function ThemedApp() {
   const classes = useStyles();
@@ -87,269 +212,76 @@ function ThemedApp() {
     setExpanded2(!expanded2);
   };
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = name => {
+    console.log(name);
+    setOpen(false);
+  };
+
+  const collegepeople = [
+    {
+      id: "jonathanshi",
+      logo: logo,
+      name: "Jonathan Shi",
+      description: "Junior at UCLA"
+    },
+    {
+      id: "waylenlam",
+      logo: logo,
+      name: "Waylen Lam",
+      description: "Senior at UCLA"
+    },
+    {
+      id: "vivianlam",
+      logo: logo,
+      name: "Vivian Lam",
+      description: "Junior at Harvey Mudd"
+    },
+    {
+      id: "fontannayee",
+      logo: logo,
+      name: "Fontanna Yee",
+      description: "Junior at UCLA"
+    }
+  ];
+
+  const highschoolpeople = [
+    {
+      fullname: "bob marks",
+      email: "bobmarks@gmail.com"
+    }
+  ];
+
   return (
     <ThemeProvider theme={appTheme}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <Typography variant="overline">
-            College Students
-          </Typography>
+          <Typography variant="overline">College Students</Typography>
         </Toolbar>
       </AppBar>
 
-      <Grid
-        container
-        direction="row"
-        justify="space-evenly"
-        alignItems="flex-start"
-      >
-
-        {/* Jonath Shi */}
-        <Grid item xs={12} sm={6} md={4}>
-
-          <Card className={classes.root}>
-            <CardHeader
-              title="Jonathan Shi"
-              subheader="College Student at UCLA"
-            />
-            <CardMedia
-              component="img"
-              height="200"
-              image={logo}
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-              
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded1,
-                })}
-                onClick={handleExpandClick1}
-                aria-expanded={expanded1}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={expanded1} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                Email: jshi3512@g.ucl.edu
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                Major: 
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                About Jonathan: 
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justify="space"
+          alignItems="flex-start"
+          spacing={4}
+          className={classes.gridthing}
+        >
+          {collegepeople.map(person => (
+            <GridPerson person={person} />
+          ))}
         </Grid>
-
-        {/* Waylen Lam */}
-        <Grid item xs={12} sm={6} md={4}>
-
-          <Card className={classes.root}>
-            <CardHeader
-              title="Waylen Lam"
-              subheader="College Student at UCLA"
-            />
-            <CardMedia
-              component="img"
-              height="200"
-              image={logo}
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-              
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded2,
-                })}
-                onClick={handleExpandClick2}
-                aria-expanded={expanded2}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={expanded2} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                Email: waylen.lam@gmail.com
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                Major: 
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                About Waylen: 
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-        </Grid>
-
-        {/* Vivian Lam */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root} variant="outlined">
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Vivian Lam
-                </Typography>
-                <Typography variant="body2" color="black" component="p">
-                  College Student at Harvey Mudd
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Email: vivianhlam00@gmail.com
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Major:
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  About Vivian
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-
-        {/* Fontanna Shee */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root} variant="outlined">
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Fontanna Shee
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  College Student at UCLA
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-
-        {/* Caleb Kang */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root} variant="outlined">
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Caleb Kang
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  College Student at UCSD
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-
-        {/* Kelalni Luong-Kha */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root} variant="outlined">
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Kelalni Luong-Kha
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  College Student at UCLA
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-
-        {/* Alvin Vo */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.root} variant="outlined">
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Alvin Vo
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  College Student at UCLA
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Connect
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-
-
-
-      </Grid>
-      
-
+      </Container>
     </ThemeProvider>
-
   );
 }
-
 
 function App() {
   const classes = useStyles();
